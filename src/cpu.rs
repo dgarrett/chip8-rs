93296c951bdf6e1f141ffbfee1d1c51766a408f1
Dylan Garrett <dylan.garrett@gmail.com>
2023-08-01 21:58:45 -0700

    Implement some opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,14 @@
+pub const WIDTH: usize = 640;
+pub const HEIGHT: usize = 360;
+
 pub struct CPU {
     reg: [u8; 16],
     pc: usize,
     mem: [u8; 0x1000],
     stack: [u16; 16],
     sp: usize,
+    i: u16,
+    disp: [u32; WIDTH * HEIGHT],
 }
 
 impl CPU {
@@ -14,6 +19,8 @@ impl CPU {
             mem: [0; 4096],
             stack: [0; 16],
             sp: 0,
+            i: 0,
+            disp: [0; WIDTH * HEIGHT],
         }
     }
 
@@ -26,28 +33,70 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
-        loop {
-            let opcode = self.read_opcode();
-            self.pc += 2;
+        while self.step() {}
+    }
 
-            let c = ((opcode & 0xF000) >> 12) as u8;
-            let x = ((opcode & 0x0F00) >> 8) as u8;
-            let y = ((opcode & 0x00F0) >> 4) as u8;
-            let d = ((opcode & 0x000F) >> 0) as u8;
+    pub fn step(&mut self) -> bool {
+        let opcode = self.read_opcode();
+        self.pc += 2;
 
-            let nnn = opcode & 0x0FFF;
-            // let kk = (opcode & 0x00FF) as u8;
+        let c = ((opcode & 0xF000) >> 12) as u8;
+        let x = ((opcode & 0x0F00) >> 8) as u8;
+        let y = ((opcode & 0x00F0) >> 4) as u8;
+        let d = ((opcode & 0x000F) >> 0) as u8;
 
-            match (c, x, y, d) {
-                (0, 0, 0, 0) => {
-                    return;
-                }
-                (0, 0, 0xE, 0xE) => self.ret(),
-                (0x2, _, _, _) => self.call(nnn),
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
-                _ => todo!("opcode {:04x}", opcode),
+        let nnn = opcode & 0x0FFF;
+        let kk = (opcode & 0x00FF) as u8;
+
+        match (c, x, y, d) {
+            (0, 0, 0, 0) => {
+                return false;
             }
+            (0, 0, 0xE, 0) => self.disp_clear(),
+            (0, 0, 0xE, 0xE) => self.ret(),
+            (0x1, _, _, _) => self.jump(nnn),
+            (0x2, _, _, _) => self.call(nnn),
+            (0x3, _, _, _) => self.eq(x, kk),
+            (0x4, _, _, _) => self.neq(x, kk),
+            (0x5, _, _, 0) => self.eq_xy(x, y),
+            (0x6, _, _, _) => self.set(x, kk),
+            (0x7, _, _, _) => self.inc(x, kk),
+            (0x8, _, _, 0) => self.assn(x, y),
+            (0x8, _, _, 0x1) => self.or_xy(x, y),
+            (0x8, _, _, 0x2) => self.and_xy(x, y),
+            (0x8, _, _, 0x3) => self.xor_xy(x, y),
+            (0x8, _, _, 0x4) => self.add_xy(x, y),
+            (0x8, _, _, 0x5) => self.sub_xy(x, y),
+            (0x8, _, _, 0x6) => self.rshift(x),
+            (0x8, _, _, 0x7) => self.isub_xy(x, y),
+            (0x8, _, _, 0xE) => self.lshift(x),
+            (0x9, _, _, 0) => self.neq_xy(x, y),
+            (0xA, _, _, _) => self.set_i(nnn),
+            (0xB, _, _, _) => self.jmp_off(nnn),
+            (0xC, _, _, _) => self.rand(x, kk),
+            (0xD, _, _, _) => self.draw(x, y, d),
+            _ => todo!("opcode {:04x}", opcode),
         }
+
+        true
+    }
+
+    fn disp_clear(&mut self) {
+        self.disp = [0; WIDTH * HEIGHT];
+    }
+
+    fn ret(&mut self) {
+        if self.sp == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.sp -= 1;
+        let addr = self.stack[self.sp];
+        self.pc = addr as usize;
+    }
+
+    fn jump(&mut self, addr: u16) {
+        self.pc = addr as usize;
     }
 
     fn call(&mut self, addr: u16) {
@@ -63,14 +112,60 @@ impl CPU {
         self.pc = addr as usize;
     }
 
-    fn ret(&mut self) {
-        if self.sp == 0 {
-            panic!("Stack underflow")
+    fn eq(&mut self, x: u8, kk: u8) {
+        let x_val = self.reg[x as usize];
+        if x_val == kk {
+            self.pc += 1;
         }
+    }
 
-        self.sp -= 1;
-        let addr = self.stack[self.sp];
-        self.pc = addr as usize;
+    fn neq(&mut self, x: u8, kk: u8) {
+        let x_val = self.reg[x as usize];
+        if x_val != kk {
+            self.pc += 1;
+        }
+    }
+
+    fn eq_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+        if x_val == y_val {
+            self.pc += 1;
+        }
+    }
+
+    fn set(&mut self, x: u8, kk: u8) {
+        self.reg[x as usize] = kk;
+    }
+
+    fn inc(&mut self, x: u8, kk: u8) {
+        self.reg[x as usize] += kk;
+        // no flag
+    }
+
+    fn assn(&mut self, x: u8, y: u8) {
+        self.reg[x as usize] = self.reg[y as usize];
+    }
+
+    fn or_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        self.reg[x as usize] = x_val | y_val;
+    }
+
+    fn and_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        self.reg[x as usize] = x_val & y_val;
+    }
+
+    fn xor_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        self.reg[x as usize] = x_val ^ y_val;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -81,6 +176,85 @@ impl CPU {
         self.reg[x as usize] = val;
 
         self.reg[0xF] = overflow as u8;
+    }
+
+    fn sub_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        let (val, overflow) = x_val.overflowing_sub(y_val);
+        self.reg[x as usize] = val;
+
+        self.reg[0xF] = overflow as u8;
+    }
+
+    fn rshift(&mut self, x: u8) {
+        let x_val = self.reg[x as usize];
+
+        self.reg[0xF] = x_val & 0x1;
+        self.reg[x as usize] = x_val >> 1;
+    }
+
+    fn isub_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        let (val, overflow) = y_val.overflowing_sub(x_val);
+        self.reg[x as usize] = val;
+
+        self.reg[0xF] = overflow as u8;
+    }
+
+    fn lshift(&mut self, x: u8) {
+        let x_val = self.reg[x as usize];
+
+        self.reg[0xF] = x_val & 0x80;
+        self.reg[x as usize] = x_val << 1;
+    }
+
+    fn neq_xy(&mut self, x: u8, y: u8) {
+        let x_val = self.reg[x as usize];
+        let y_val = self.reg[y as usize];
+
+        if x_val != y_val {
+            self.pc += 1;
+        }
+    }
+
+    fn set_i(&mut self, nnn: u16) {
+        self.i = nnn;
+    }
+
+    fn jmp_off(&mut self, nnn: u16) {
+        self.pc = self.reg[0] as usize + nnn as usize;
+    }
+
+    fn rand(&mut self, x: u8, kk: u8) {
+        self.reg[x as usize] = rand::random::<u8>() & kk;
+    }
+
+    fn set_px(&mut self, x: u8, y: u8, set: bool) -> bool {
+        let pixel = &mut self.disp[x as usize + (y as usize * WIDTH)];
+        let was_set = *pixel == 0xffff;
+
+        *pixel = if set { 0xffff } else { 0 };
+
+        was_set && (*pixel == 0)
+    }
+
+    fn draw(&mut self, x: u8, y: u8, d: u8) {
+        self.reg[0xF] = 0;
+
+        for y in y..(y + d) {
+            let row_pxls = self.mem[self.i as usize + y as usize];
+            for x in x..(x + 8) {
+                let set = (row_pxls & (0x8 >> x)) != 0;
+                let unset = self.set_px(x, y, set);
+                if unset {
+                    self.reg[0xF] = 1;
+                }
+            }
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 mod cpu;
 
-use crate::cpu::CPU;
+use crate::cpu::{CPU, HEIGHT, WIDTH};
+
+use minifb::{Key, Window, WindowOptions};
+
+// fn main() {
+//     let mut cpu = CPU::new();
+//     cpu.run();
+// }
 
 fn main() {
-    let mut cpu = CPU::new();
-    cpu.run();
+    let _cpu = CPU::new();
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0xff00ff; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
